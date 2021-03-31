881024b9d5fc022d6179160ea57142a83413e40d
robinsonvs <robinsonvs@gmail.com>
2021-03-31 15:46:20 -0300

    update BeerServiceTest - add BeerDTOBuilder

--- src/test/java/one/digitalinnovation/beerstock/service/BeerServiceTest.java
+++ src/test/java/one/digitalinnovation/beerstock/service/BeerServiceTest.java
@@ -1,7 +1,48 @@
 package one.digitalinnovation.beerstock.service;
 
+import one.digitalinnovation.beerstock.dto.BeerDTO;
+import one.digitalinnovation.beerstock.entity.Beer;
+import one.digitalinnovation.beerstock.exception.BeerAlreadyRegisteredException;
+import one.digitalinnovation.beerstock.mapper.BeerMapper;
+import one.digitalinnovation.beerstock.repository.BeerRepository;
+import one.digitalinnovation.beerstock.service.builder.BeerDTOBuilder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class BeerServiceTest {
+
+    private static final long INVALID_BEER_ID = 1L;
+
+    @Mock
+    private BeerRepository beerRepository;
+
+    private BeerMapper beerMapper = BeerMapper.INSTANCE;
+
+    @InjectMocks
+    private BeerService beerService;
+
+    @Test
+    void whenBeerInformedThenItShouldBeCreated() throws BeerAlreadyRegisteredException {
+        //given
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer beerExpected = beerMapper.toModel(beerDTO);
+        //when
+        Mockito.when(beerRepository.findByName(beerDTO.getName())).thenReturn(Optional.empty());
+        Mockito.when(beerRepository.save(beerExpected)).thenReturn(beerExpected);
+        //then
+        BeerDTO beerCreated = beerService.createBeer(beerDTO);
+
+        assertEquals(beerDTO.getId(), beerCreated.getId());
+        assertEquals(beerDTO.getName(), beerCreated.getName());
+    }
 
 }
